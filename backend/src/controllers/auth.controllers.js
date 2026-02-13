49e66e38b72bc8b2d9336dbe843be8827d4679d5
dinesh2432 <ddk113311@gmail.com>
2026-02-13 14:18:32 +0530

    added login and logout controllers and routes

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const bcryptjs = require("bcryptjs")
+const bcrypt = require("bcryptjs")
 const jwt = require('jsonwebtoken')
 const crypto = require('crypto')
 const userModel = require("../models/userSchema")
@@ -8,6 +8,7 @@ require('dotenv').config()
 
 const signup = async(req,res)=>{
     const {name,email,password}=req.body
+    //todo : check regex expression for email here 
     if(!name || !email ||!password){
         return res.status(400).json({message:"Data missing!!"})
 
@@ -17,11 +18,11 @@ const signup = async(req,res)=>{
         if(existingUser){
             return res.status(400).json({message:"User already exists!!"})
         }
-        const hashPassword = await bcryptjs.hash(password,10);
+        const hashPassword = await bcrypt.hash(password,10);
         const user = new userModel({
             name:name,
             email:email,
-            password:password
+            password:hashPassword
         })
         await user.save()
         const token = jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:'7d'})
@@ -40,5 +41,53 @@ const signup = async(req,res)=>{
 
 }
 
+const login = async(req,res)=>{
+    const {email,password}=req.body
+    if(!email || !password){
+        return res.status(400).json({message:"Data missing!!"})
+    }
+    //todo: do email regex for login
+    try{
+        const user = await userModel.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"User already exists"})
+        }
+        const decode = await bcrypt.compare(password,user.password)
+        if(!decode){
+            return res.status(400).json({message:"Invalid passsword!!"})
+        }
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET_KEY,{expiresIn:'7d'})
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure:true,
+            sameSite:"None",
+            maxAge:7*24*60*60*1000
+            
+        })
+        return res.status(200).json({message:"login success",token})
 
-module.exports={signup}
+    }catch(err){
+        return res.status(400).json({message:err.message})
+    }
+}
+
+
+const logout = async(req,res)=>{
+
+    try{
+        res.clearCookie("token",{
+            httpOnly:true,
+            secure:process.env.NODE_ENV ==='production',
+            sameSite:process.env.NODE_ENV ==='production' ? 'none' : 'strict',
+        })
+        return res.status(200).json({message:"Logout successfully"})
+
+    }catch(err){
+        return res.status(400).json({message:err.message})
+    }
+}
+
+
+
+
+module.exports={signup,login,logout}
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const { signup } = require("../controllers/auth.controllers")
+const { signup, login, logout } = require("../controllers/auth.controllers")
 const authroute = express.Router()
 
 
 authroute.post("/signup",signup)
+authroute.post("/login",login)
+authroute.post("/logout",logout)
 
 
 module.exports=authroute
